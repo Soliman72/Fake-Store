8512bbbcac961646bacaedb2cbbfca3af8e486c4
Soliman72 <solimanelaraby743@gmail.com>
2024-07-04 14:41:28 +0300

    update seeder

--- seeds/seeder.js
+++ seeds/seeder.js
@@ -33,22 +33,27 @@ const db = require('../config/db');
 
 async function seedCategories() {
   // Insert categories
-  db.query('INSERT INTO categories (name) VALUES (?)', ["categoryName"] ,(err)=>{
-    if ( err ) throw err;
-    console.log('Categories seeded successfully');
-  });
+  for ( let i = 0; i < 10; i++ ){
+    db.query( 'INSERT INTO categories (name) VALUES (?)', [ "categoryName" ], ( err ) =>{
+      if ( err ) throw err;
+      console.log( 'Categories seeded successfully' );
+    } );
+  }
 }
 
 async function seedProducts() {
     // Insert products
-  db.query( "SET FOREIGN_KEY_CHECKS=0" );
-  db.query( `
+  for ( let i = 0; i < 100; i++ ){
+    db.query( "SET FOREIGN_KEY_CHECKS=0" );
+    db.query( `
     INSERT INTO products( title, description, price, category_id ) VALUES(?, ?, ? , ?)`,
-    [ "product title" , "product description" , 4000 , 39  ]  , (err)=>{
-      if ( err ) throw err;
-      console.log('Products seeded successfully');
-    });
+      [ "product title", "product description", 4000, 39 ], ( err ) =>{
+        if ( err ) throw err;
+        console.log( 'Products seeded successfully' );
+      } );
+  }
 }
+
 async function runSeed() {
   try {
     seedCategories();
